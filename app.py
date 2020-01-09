4430ca34eb12565927d4c33e2102e69e80098516
Anastasia Tuzhilina <haziran@live.ie>
2020-01-09 10:44:11 +0000

    Connect to MongoDB Atlas

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+
+if path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv("MONGO_URI", 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/home')
-def home():
-    return 'Home Page'
+@app.route('/get_books')
+def get_books():
+    return render_template("get_books.html", books=mongo.db.books.find())
 
 
 if __name__ == '__main__':
