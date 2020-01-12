a6db0c9b3071111e5891f557c19ed76155e1f051
Anastasia Tuzhilina <haziran@live.ie>
2020-01-12 18:54:33 +0000

    Styling add_list.html

--- static/js/page.js
+++ static/js/page.js
@@ -1,4 +1,4 @@
- //Jquery for action buttons
+ //Jquery for list action buttons
 $( document ).ready(function(){
     $(".list-action-btn").hover(function(){
         $(this).toggleClass("list-action-btn-active");
@@ -12,4 +12,9 @@ $( document ).ready(function(){
         $(this).toggleClass("lists-list-row-active");
         $(this).children("div").children("a").toggleClass("list-item-active");
     });
-})   
+  
+     //Jquery for 'back to lists' button
+    $(".action-btn-back").hover(function(){
+        $(this).toggleClass("action-btn-back-active");
+    });
+})
