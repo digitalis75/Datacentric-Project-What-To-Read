5373c08376ca553dce7bbce69f3f93a7094586be
Anastasia Tuzhilina <haziran@live.ie>
2020-01-18 20:33:58 +0000

    Add Home button in navbar, remove JS from navbar buttons

--- static/js/page.js
+++ static/js/page.js
@@ -1,12 +1,4 @@
- //Jquery for list action buttons
 $( document ).ready(function(){
-    $(".list-action-btn").hover(function(){
-        $(this).toggleClass("list-action-btn-active");
-    });
-
-    $(".action-btn").hover(function(){
-        $(this).toggleClass("action-btn-active");
-    });
 //JQuery for list-row
     $(".lists-list-row").hover(function(){
         $(this).toggleClass("lists-list-row-active");
