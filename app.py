3b8c72a64aad8ac5d8ca82c1b23a1b688393b979
Anastasia Tuzhilina <haziran@live.ie>
2020-02-26 22:12:01 +0000

    Debug mode is disabled

--- app.py
+++ app.py
@@ -335,4 +335,4 @@ def insert_book():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
